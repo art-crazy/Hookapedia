d01386c80bf7793608f7360710f6b860ff9b54c6
arzhannikov <artcrazy.mail@gmail.com>
2025-07-20 03:04:04 +0300

    ref

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
 import { Recipe } from './recipes';
 import { summerHome } from './recipes-home/summerHome';
 
-export const    recipes: Record<number, Recipe> = {
+export const recipes: Record<number, Recipe> = {
     ...summerHome
 };
