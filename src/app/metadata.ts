5ae8fb442642aba29841f05812ea76547e2c9cd5
arzhannikov <artcrazy.mail@gmail.com>
2025-11-14 19:57:48 +0300

    feat(): add favicon

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -31,15 +31,11 @@ export const metadata: Metadata = {
         icon: [
             { url: "/favicon.svg", type: "image/svg+xml" },
             { url: "/favicon.ico", type: "image/x-icon" },
-            { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
-            { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" }
+            { url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" }
         ],
         apple: [
             { url: "/apple-touch-icon.png", sizes: "180x180" }
-        ],
-        other: [
-            { rel: "android-chrome", url: "/android-chrome-192x192.png", sizes: "192x192" },
-            { rel: "android-chrome", url: "/android-chrome-512x512.png", sizes: "512x512" }
         ]
     },
+    manifest: "/site.webmanifest",
 };
